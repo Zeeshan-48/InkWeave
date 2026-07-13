f9152496411f6d3d83129b4cf7128a54238b6a83
Zeeshan Anwer <zeshan1234anwar@gmail.com>
2026-07-13 15:44:51 +0530

    Configure TinyMCE API key everywhere

--- react-blog/src/components/BlogForm.jsx
+++ react-blog/src/components/BlogForm.jsx
@@ -101,7 +101,7 @@ const BlogForm = ({ post }) => {
                                 <Editor
                                     id="content-editor"
                                     initialValue={post?.content || ''}
-                                    apiKey={import.meta.env.VITE_TINYMCE_API_KEY || "no-api-key"}
+                                    apiKey={import.meta.env.VITE_TINYMCE_API_KEY || "4ga9bv5rjovwvkyax0dcefng7azpapbweq38tnt952bjc16p"}
                                     init={{
                                         height: 350,
                                         menubar: false,
